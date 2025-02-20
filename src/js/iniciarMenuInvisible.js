86192a0b165f54090bf2d58605043eb5521a463b
FacuAmelotti <facuezequielamelotti@gmail.com>
2025-02-20 16:14:40 -0300

    New commit 20/2.2

---
+++ src/js/iniciarMenuInvisible.js
@@ -0,0 +1,8 @@
+// Llamar a la función para ocultar el menú cuando la página cargue
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+    const content = document.querySelector('.content');
+
+        desplegarAction();
+
+});
--- src/js/menuDesplegable.js
+++ src/js/menuDesplegable.js
@@ -16,16 +16,34 @@ function desplegarAction() {
     if (menu.classList.contains('active')) {
         content.style.width = '76%'; // Restaurar ancho original
         content.style.marginLeft = '24%'; // Restaurar margen izquierdo
-        menuToggle.textContent = 'Esconder'; // Cambiar texto del botón
+        menuToggle.textContent = 'X'; // Cambiar texto del botón
 
     } else {
         content.style.width = '100%'; // Expandir al 100%
         content.style.marginLeft = '0'; // Eliminar margen izquierdo
-        menuToggle.textContent = '☰'; // Cambiar texto del botón        
+        menuToggle.textContent = '☰ '; // Cambiar texto del botón        
     }
 }
 
 // Iniciar con el menú desplegado
 document.addEventListener('DOMContentLoaded', () => {
     desplegarAction();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.querySelector(".content");
+
+    content.addEventListener("animationend", () => {
+        content.classList.add("animated"); // Activa el scroll después de la animación
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".desplegable > a").forEach(item => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault(); // Evita que el enlace recargue la página
+            const parentLi = item.parentElement;
+            parentLi.classList.toggle("open");
+        });
+    });
 });
